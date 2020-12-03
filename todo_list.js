cf1c99feacf7a26b51799ffbef7c633a21b76382
lrmcc <lmcconnelldev@gmail.com>
2020-12-02 20:14:08 -0700

    add and remove items functions properly

--- todo_list.js
+++ todo_list.js
@@ -1,18 +1,34 @@
-let todoList = []
+let entryCount = 1;
+let entries = [];
 
-function getTodoItem(todoItem){
-    var todoItem = document.getElementById("todoItem").value;
-    todoList += todoItem;
+function getTodoItem(){
+    var todoItem = document.getElementById("todoItemTextBox").value;
+    entries.push(todoItem);
     putTodoItem(todoItem);
 } 
 
 function putTodoItem(todoItem){
+    entryCount = entries.length;
     var completelist = document.getElementById("todoItemList");
-    completelist.innerHTML += "<li>Item " + todoItem + "</li>";
+    completelist.innerHTML += "<li id=" + entryCount +">" + entryCount +". " + todoItem + "<button type='button' class='clearTodoItem' id=" + entryCount +" onclick='clearTodoItem(" + entryCount + ")'>x</button>" + "</li>";
 } 
 
-function clearTodoItem(){
+function clearTodoItem(entryCount){
+    entries = entries.filter(a => a !== entries[entryCount-1]);
     var completelist = document.getElementById("todoItemList");
+    completelist.innerHTML = ""
+    entryCount = 1;
+    for (let i = 0; i < entries.length; i++){
+        completelist.innerHTML += "<li id=" + entryCount +">" + entryCount +". " + entries[i] + "<button type='button' class='clearTodoItem' id=" + entryCount +" onclick='clearTodoItem(" + entryCount + ")'>x</button>" + "</li>";
+        entryCount++;
+    }
+}
+
+function clearTodoList(){
+    var completelist = document.getElementById("todoItemList");
+    entries = [];
+    entryCount = 0;
     completelist.innerHTML = "";
 }
+
 
