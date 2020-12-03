23f62d3fd336d2e155233e9acaf6c7423fa871fb
lrmcc <lmcconnelldev@gmail.com>
2020-12-03 00:47:08 -0700

    download list as csv; strikethrough text added

--- todo_list.js
+++ todo_list.js
@@ -1,6 +1,7 @@
 let entryCount = 1;
 let entries = [];
-
+let done = "<button type='button' class='todoListItemDoneButton' id=" + entryCount +" onclick='todoListItemDone(" + entryCount + ")'>Done</button>"
+let clearItem = "<button type='button' class='clearTodoItemFromListButton' id=" + entryCount +" onclick='clearTodoItemFromList(" + entryCount + ")'>x</button>";
 
 function submitWithReturn(event) {
     if (event.keyCode === 13) {
@@ -18,24 +19,48 @@ function getTodoItem(){
 function putTodoItem(todoItem){
     entryCount = entries.length;
     var completelist = document.getElementById("todoItemList");
-    completelist.innerHTML += "<li id=" + entryCount +">" + entryCount +". " + todoItem + "<button type='button' class='clearTodoItemFromListButton' id=" + entryCount +" onclick='clearTodoItemFromList(" + entryCount + ")'>x</button>" + "</li><hr>";
+    completelist.innerHTML += "<li onclick='todoListItemDone(this)' id=" + entryCount +">" + entryCount +". " + todoItem + clearItem + done + "</li><hr>";
 } 
 
-function clearTodoItemFromList(entryCount){
-    entries = entries.filter(a => a !== entries[entryCount-1]);
+function todoListItemDone(item) {
+    let txtDec = item.style.textDecoration;
+    if (txtDec === "line-through"){
+        item.style = "text-decoration:none;";
+    }else{
+      item.style = "text-decoration:line-through;";  
+    }
+    
+  }
+
+function exportTodoList(){
+    let csvContent = "data:text/csv;charset=utf-8," + entries.toString();
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "todo_data.csv");
+    document.body.appendChild(link);
+    link.click(); 
+}
+
+function clearTodoItemFromList(itemEntryCount){
+    entries = entries.filter(a => a !== entries[itemEntryCount-1]);
     var completelist = document.getElementById("todoItemList");
     completelist.innerHTML = ""
     entryCount = 1;
     for (let i = 0; i < entries.length; i++){
-        completelist.innerHTML += "<li id=" + entryCount +">" + entryCount +". " + entries[i] + "<button type='button' class='clearTodoItemFromListButton' id=" + entryCount +" onclick='clearTodoItemFromList(" + entryCount + ")'>x</button>" + "</li><hr>";
+        completelist.innerHTML += "<li onclick='todoListItemDone(this)' id=" + entryCount +">" + entryCount +". " + entries[i] + clearItem + done + "</li><hr>";
         entryCount++;
     }
 }
 
 function clearTodoList(){
-    var completelist = document.getElementById("todoItemList");
-    entries = [];
-    entryCount = 0;
-    completelist.innerHTML = "";
+    let choice = confirm("Are you sure you want to clear the list?");
+    if (choice){
+        var completelist = document.getElementById("todoItemList");
+        entries = [];
+        entryCount = 0;
+        completelist.innerHTML = "";
+    }
+    
 }
 
